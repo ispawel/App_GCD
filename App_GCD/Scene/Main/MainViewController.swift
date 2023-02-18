07b6e3c677a6cefa9cca0ce1af48be12855e09e9
Isakov_Pavel <isapawel@gmail.com>
2023-02-18 23:15:02 +0700

    Initial commit

---
+++ App_GCD/Scene/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  App_GCD
+//
+//  Created by Pavel Isakov on 18.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
